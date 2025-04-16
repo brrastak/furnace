ab35bccc22336ccf19484d0547bbce14e2fe5b05
Yuri Volkov <volkov.yuri.alex@gmail.com>
2025-04-16 23:02:18 +0200

    Check CRC8 algorithms

--- furnace_230V_FW/src/crc.rs
+++ furnace_230V_FW/src/crc.rs
@@ -13,3 +13,36 @@ pub const CRC8_ANSI: Algorithm<u8> = Algorithm {
     check: 0xF4, // CRC-8 for "123456789"
     residue: 0x00,
 };
+
+pub const CRC8_DALLAS: Algorithm<u8> = Algorithm {
+    width: 8,
+    poly: 0x31,       // Note: Your function uses 0x8C due to bitwise LSB-first logic, but algorithmically it's 0x31
+    init: 0x00,
+    refin: true,      // Processes input bits LSB-first
+    refout: false,    // Output is NOT reflected in your function
+    xorout: 0x00,
+    check: 0xA1,      // This matches the standard check value for "123" (0x31, 0x32, 0x33)
+    residue: 0x00,
+};
+
+pub const CRC_8_MAXIM_DOW: Algorithm<u8> = Algorithm { 
+    width: 8, 
+    poly: 0x31, 
+    init: 0x00, 
+    refin: true, 
+    refout: true, 
+    xorout: 0x00, 
+    check: 0xa1, 
+    residue: 0x00 
+};
+
+pub const CRC_8_NRSC_5: Algorithm<u8> = Algorithm { 
+    width: 8, 
+    poly: 0x31, 
+    init: 0xff, 
+    refin: false, 
+    refout: false, 
+    xorout: 0x00, 
+    check: 0xf7, 
+    residue: 0x00 
+};
